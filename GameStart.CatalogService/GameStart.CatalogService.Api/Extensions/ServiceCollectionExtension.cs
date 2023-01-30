cd514e1ad2e35bf8056fa1b7a367f0382a0d4157
Dzmitry Tsykman <dimatsikman@gmail.com>
2023-01-30 13:05:41 +0300

    Add db connection and auth.

--- GameStart.CatalogService/GameStart.CatalogService.Api/Extensions/ServiceCollectionExtension.cs
+++ GameStart.CatalogService/GameStart.CatalogService.Api/Extensions/ServiceCollectionExtension.cs
@@ -1,19 +1,81 @@
 ﻿using GameStart.CatalogService.Data;
+using GameStart.CatalogService.Data.EntityConfigurations.ValueConverters;
+using GameStart.CatalogService.Data.Models;
+using GameStart.CatalogService.Data.Repositories;
 using GameStart.Shared;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace GameStart.CatalogService.Api.Extensions
 {
     public static class ServiceCollectionExtension
     {
-        public static void AddDbContextsWithIdentity(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddDbContextsWithRepositories(this IServiceCollection services, IConfiguration configuration)
         {
-            static void ConfigureDbContext(DbContextOptionsBuilder options, string connectionString) =>
-                options.UseSqlServer(connectionString, o => o.MigrationsAssembly(typeof(CatalogDbContext).Assembly.FullName));
-
             var catalogConnectionString = configuration.GetConnectionString(Constants.CatalogService.ConnectionStringNames.CatalogDb);
 
-            services.AddDbContext<CatalogDbContext>(options => ConfigureDbContext(options, catalogConnectionString));
+            services.AddDbContext<CatalogDbContext>(options =>
+            {
+                options.UseSqlServer(catalogConnectionString, cfg => cfg.MigrationsAssembly(typeof(CatalogDbContext).Assembly.FullName));
+            });
+
+            services.AddScoped<IRepository<VideoGame>, VideoGameRepository>();
+            services.AddScoped<IRepository<Language>, LanguageRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddPreconfiguredAuthentication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+                {
+                    options.TokenValidationParameters.ValidateAudience = false;
+                    options.Authority = configuration["Auth:Authority"];
+                });
+
+            services.AddAuthorization();
+
+            // override default behavior
+            services.ConfigureApplicationCookie(o =>
+            {
+                o.Events.OnRedirectToLogin = (ctx) =>
+                {
+                    if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode < 400)
+                    {
+                        ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    }
+
+                    return Task.CompletedTask;
+                };
+                o.Events.OnRedirectToAccessDenied = (ctx) =>
+                {
+                    if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode < 400)
+                    {
+                        ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
+                    }
+
+                    return Task.CompletedTask;
+                };
+            });
+
+            return services;
+        }
+
+        public static IServiceCollection AddControllersWithJsonOptions(this IServiceCollection services)
+        {
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+                    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+                    //options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
+                });
+
+            return services;
         }
     }
 }
